1b6d5f9023765a2d1955de6efc19b3b1f3d1c374
Tim Molloy <service@salient5.com>
2016-09-29 01:17:22 -0500

    initial project version

---
+++ RfReceive24.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		RfReceive24.ino
+ Created:	9/29/2016 1:13:46 AM
+ Author:	Tim
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
